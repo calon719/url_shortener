87063ecf17dbdb2a4b07c73a3374e07cf28acfdc
Calon <calonhu@protonmail.com>
2023-09-15 16:33:19 +0800

    Add shorten function

--- app.js
+++ app.js
@@ -1,23 +1,59 @@
 const express = require('express');
 const { engine } = require('express-handlebars');
+const fs = require('fs');
 
 const app = express();
 const port = 3000;
-const urlList = require('./public/jsons/urls.json');
+const URL_LIST_PATH = './public/jsons/urls.json';
+const urlList = require(URL_LIST_PATH);
+let isShortened = false;
 
 app.engine('.hbs', engine({ extname: '.hbs' }));
 app.set('view engine', '.hbs');
 app.set('views', './views');
 
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', { isShortened });
+});
+
+app.post('/', (req, res) => {
+  const { url } = req.body;
+  const existedURL = urlList.find((item) => item.url === url);
+  let shortenedURL = 'https://localhost/';
+
+  if (existedURL === undefined) {
+    const id = Math.random().toString(36).substring(2, 7);
+    const data = {
+      id,
+      url,
+    };
+
+    urlList.push(data);
+    fs.writeFile(URL_LIST_PATH, JSON.stringify(urlList), (err) => {
+      if (err) {
+        throw err;
+      }
+    });
+
+    shortenedURL += id;
+  } else {
+    shortenedURL += existedURL.id;
+  }
+
+  isShortened = true;
+  res.render('index', {
+    shortenedURL,
+    isShortened,
+  });
 });
 
 app.get('/:id', (req, res) => {
   const { id } = req.params;
 });
+
 
 app.listen(port, () => {
   console.log(`URL Shortener server is running on http://localhost:${port}`);
